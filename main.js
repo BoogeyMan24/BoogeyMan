fdf30b7b94f625460a72b4f22fd04bf1a0489368
BoogeyMan <76151726+BoogeyMan24@users.noreply.github.com>
2022-05-03 00:44:45 +0200

    Fixes and Updates

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const backgroundNum = Math.floor(Math.random() * (3 - 1) + 1); // I’m adding .5 here so the flicker isn’t too obnoxious
-
+const backgroundNum = Math.floor(Math.random() * (1001 - 1) + 1); // I’m adding .5 here so the flicker isn’t too obnoxious
 console.log(backgroundNum);
 
+window.onload = function(){
+    const background = document.getElementById('background');
 
-const background = document.querySelector('body');
+    console.log(background)
 
-if(backgroundNum == 1)
-{
-    background.style.setProperty('background', "url('../images/amongus.png')");
-}
-else if(backgroundNum == 2)
-{
-    background.style.setProperty('background', "url('../images/susplanet.png')");
-}
+    if(backgroundNum == 1)
+    {
+        background.style.setProperty('--background', "url('../images/amongus.png')");
+    }
+    else if(backgroundNum >= 2)
+    {
+        background.style.setProperty('--background', "url('../images/susplanet.png')");
+    }
+};
