ed096d0c630500a10c1c9624fb3988f059017db3
BoogeyMan24 <peta.hora11@gmail.com>
2022-05-18 17:03:12 +0200

    0.1.2.6

---
+++ about.js
@@ -0,0 +1,29 @@
+document.addEventListener('keydown', function(event) {
+    if(event.key == "1") {
+        document.documentElement.style.setProperty("--speed-underline", "0.1s");
+    }
+    if(event.key == "2") {
+        document.documentElement.style.setProperty("--speed-underline", "0.2s");
+    }   
+    if(event.key == "3") {
+        document.documentElement.style.setProperty("--speed-underline", "0.3s");
+    }
+    if(event.key == "4") {
+        document.documentElement.style.setProperty("--speed-underline", "0.4s");
+    }
+    if(event.key == "5") {
+        document.documentElement.style.setProperty("--speed-underline", "0.5s");
+    }   
+    if(event.key == "6") {
+        document.documentElement.style.setProperty("--speed-underline", "0.6s");
+    }
+    if(event.key == "7") {
+        document.documentElement.style.setProperty("--speed-underline", "0.7s");
+    }   
+    if(event.key == "8") {
+        document.documentElement.style.setProperty("--speed-underline", "0.8s");
+    }
+    if(event.key == "9") {
+        document.documentElement.style.setProperty("--speed-underline", "0.9s");
+    }   
+});
